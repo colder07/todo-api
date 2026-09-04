d66253a7cd73dbf1c824a9a54d6004f24b3cb0b4
colder07 <ke_hanhan@outlook.com>
2026-09-04 19:05:23 +0800

    feat:add todo create endpoint

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -8,4 +8,19 @@ class Api::V1::TodosController < ApplicationController
     todo = Todo.find(params[:id])
     render json: todo
   end
+
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: todo, status: :created
+    else
+      render json: todo.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :description, :completed)
+  end
 end
