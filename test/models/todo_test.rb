82b99ab3f167b727ef7e22805dccf436c7aaec2b
colder07 <ke_hanhan@outlook.com>
2026-09-04 21:10:31 +0800

    test: add todo validation tests

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with a title" do
+  todo = Todo.new(title: "Test Todo")
+  assert todo.valid?
+  end
+
+  test "is invalid without a title" do
+    todo = Todo.new(title: nil)
+    assert todo.invalid?
+  end
 end
