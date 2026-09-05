9eaaa05a56900a3988a91ab7c01e8fe4600703b3
colder07 <ke_hanhan@outlook.com>
2026-09-05 15:28:26 +0800

    test: add todo API show test

--- test/controllers/api/v1/todos_controller_test.rb
+++ test/controllers/api/v1/todos_controller_test.rb
@@ -20,4 +20,35 @@ class Api::V1::TodosControllerTest < ActionDispatch::IntegrationTest
     assert_not_nil returned_todo
     assert_equal todo.title, returned_todo["title"]
   end
+
+  test "GET /api/v1/todos/:id returns a todo" do
+    todo = Todo.create!(
+      title: "Test Todo",
+      description: "This is a test todo",
+      completed: false
+    )
+
+    get "/api/v1/todos/#{todo.id}"
+    assert_response :ok
+
+    body = JSON.parse(response.body)
+
+    assert_equal todo.id, body["id"]
+    assert_equal todo.title, body["title"]
+  end
+
+  test "GET /api/v1/todos/:id returns 404 when todo does not exist" do
+    todo = Todo.create!(
+      title: "Test Todo",
+      description: "This is a test todo",
+      completed: false
+    )
+    todo.destroy!
+
+    get "/api/v1/todos/#{todo.id}"
+    assert_response :not_found
+
+    body = JSON.parse(response.body)
+    assert_equal "Todo not found", body["error"]
+  end
 end
