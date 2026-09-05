bd6be723207d315b8ca07951b006af59f4e8b402
colder07 <ke_hanhan@outlook.com>
2026-09-05 14:34:56 +0800

    test: add index API test for todos

--- test/controllers/api/v1/todos_controller_test.rb
+++ test/controllers/api/v1/todos_controller_test.rb
@@ -1,8 +1,23 @@
 require "test_helper"
 
 class Api::V1::TodosControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_v1_todos_index_url
-    assert_response :success
+  test "GET /api/v1/todos returns todos" do
+    todo = Todo.create!(
+      title: "Test Todo",
+      description: "This is a test todo",
+      completed: false
+    )
+
+    get "/api/v1/todos"
+    assert_response :ok
+
+    body = JSON.parse(response.body)
+
+    returned_todo = body.find do |item|
+      item["id"] == todo.id
+    end
+
+    assert_not_nil returned_todo
+    assert_equal todo.title, returned_todo["title"]
   end
 end
