a895425827471e918c5279f83288459ef36b8b6a
colder07 <ke_hanhan@outlook.com>
2026-09-07 08:01:11 +0000

    feat: add unique index to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_07_044843) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_07_075318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -26,5 +26,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_07_044843) do
     t.datetime "created_at", null: false
     t.string "email"
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
