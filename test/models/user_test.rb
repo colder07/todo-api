836147d548f700a6efe7b5d353dbc41ae4b22a83
colder07 <ke_hanhan@outlook.com>
2026-09-07 11:51:11 +0000

    refactor: update user test data

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,25 +2,23 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   test "user is valid with an email" do
-    user = User.new(email: "test@example.com")
+    user = User.new(email: "test@example.com", password: "password123")
     assert user.valid?
   end
 
   test "user is invalid without an email" do
-    user = User.new(email: "")
+    user = User.new(email: "", password: "password123")
     assert_not user.valid?
   end
 
   test "user is invalid with a duplicate email" do
-    User.create!(email: "test@example.com")
-    duplicate_user  = User.new(email: "test@example.com")
+    User.create!(email: "test@example.com", password: "password123")
+    duplicate_user  = User.new(email: "test@example.com", password: "password123")
     assert_not duplicate_user.valid?
   end
 
   test "user has many todos" do
-    user = User.create!(
-      email: "test@example.com"
-    )
+    user = User.create!(email: "test@example.com", password: "password123")
     user.todos.create!(title: "Todo 1")
     user.todos.create!(title: "Todo 2")
 
