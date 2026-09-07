c661b938077e6a2c7dea1299f406809d72df2d42
colder07 <ke_hanhan@outlook.com>
2026-09-07 08:57:36 +0000

    test: require todo user

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,15 +1,20 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  test "is valid with a title" do
+  test "todo is valid with a title" do
     user = User.create!(email: "test@example.com")
     todo = Todo.new(title: "Test Todo", user: user)
     assert todo.valid?
   end
 
-  test "is invalid without a title" do
+  test "todo is invalid without a title" do
     user = User.create!(email: "test@example.com")
     todo = Todo.new(title: nil, user: user)
+    assert todo.invalid?
+  end
+
+  test "todo is invalid without a user" do
+    todo = Todo.new(title: "Test Todo")
     assert todo.invalid?
   end
 end
