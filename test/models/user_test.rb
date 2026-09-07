e894fc43f2fb29be2bfe71532bf4f1fb76b580cf
colder07 <ke_hanhan@outlook.com>
2026-09-07 07:50:43 +0000

    test: add user email validation tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user is valid with an email" do
+    user = User.new(email: "test@example.com")
+    assert user.valid?
+  end
+
+  test "user is invalid without an email" do
+    user = User.new(email: "")
+    assert_not user.valid?
+  end
+
+  test "user is invalid with a duplicate email" do
+    User.create!(email: "test@example.com")
+    duplicate_user  = User.new(email: "test@example.com")
+    assert_not duplicate_user.valid?
+  end
 end
