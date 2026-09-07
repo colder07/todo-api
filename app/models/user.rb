568d44928c56da3186703a1a941da288b3bed9e5
colder07 <ke_hanhan@outlook.com>
2026-09-07 08:31:37 +0000

    feat: associate todos to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :todos
+
   validates :email, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_07_075318) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_07_081342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_07_075318) do
     t.text "description"
     t.string "title"
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_07_075318) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "todos", "users"
 end
