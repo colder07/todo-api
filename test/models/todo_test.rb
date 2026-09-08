756a52c9429fd2fe4c63e1ade365973467f09f04
colder07 <ke_hanhan@outlook.com>
2026-09-08 05:30:39 +0000

    refactor: update todo user test data

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -2,13 +2,13 @@ require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
   test "todo is valid with a title" do
-    user = User.create!(email: "test@example.com")
+    user = User.create!(email: "test@example.com", password: "password123")
     todo = Todo.new(title: "Test Todo", user: user)
     assert todo.valid?
   end
 
   test "todo is invalid without a title" do
-    user = User.create!(email: "test@example.com")
+    user = User.create!(email: "test@example.com", password: "password123")
     todo = Todo.new(title: nil, user: user)
     assert todo.invalid?
   end
